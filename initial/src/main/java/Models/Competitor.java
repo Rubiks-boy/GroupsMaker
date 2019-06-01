1cb9aa4695a8d4325aac3f9d1ef28c87985a22df
Rubiks-boi <adamwalker567123@gmail.com>
2019-05-31 20:18:20 -0700

    Added helping model

--- initial/src/main/java/Models/Competitor.java
+++ initial/src/main/java/Models/Competitor.java
@@ -1,6 +1,8 @@
 package Models;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -9,12 +11,14 @@ public class Competitor {
     private String wcaId;
     private LocalDate birthDate;
     private Map<String, Boolean> events;
+    private List<Helping> helpingIn;
 
     public Competitor(String name, String wcaId, LocalDate birthDate, Map<String, Boolean> events) {
         this.name = name;
         this.wcaId = wcaId;
         this.birthDate = birthDate;
         this.events = events;
+        helpingIn = new ArrayList<>();
     }
 
     public String getName() {
@@ -33,10 +37,26 @@ public class Competitor {
         return events;
     }
 
+    public void addHelping(Helping helping) {
+        helpingIn.add(helping);
+    }
+
+    public void removeHelping(Helping helping) {
+        helpingIn.remove(helping);
+    }
+
+    public List<Helping> getHelpingIn() {
+        return helpingIn;
+    }
+
+    @Override
     public String toString() {
         String str = String.format("%s %s %s", getName(), getWcaId(), getBirthDate().toString());
         for(String event : getEvents().keySet().stream().filter(event -> getEvents().get(event)).collect(Collectors.toList())) {
             str += " " + event;
+        }
+        for(Helping h : getHelpingIn()) {
+
         }
         return str;
     }
